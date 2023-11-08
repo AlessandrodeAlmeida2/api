67c99a57af62f53dea091003d5ff855edb406eea
Alessandro de Almeida <124206367+AlessandrodeAlmeida2@users.noreply.github.com>
2023-11-08 14:02:42 -0300

    Add files via upload

--- api/index.py
+++ api/index.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
+  
+@app.route("/", methods=["GET", "POST"])
+def conversor_moedas():
+    moedas_validas = ["USD", "BRL", "EUR", "JPY", "GBP"]
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+    if request.method == "POST":
+        valor = float(request.form["valor"])
+        moeda_origem = request.form["moeda_origem"]
+        moeda_destino = request.form["moeda_destino"]
 
-@app.route('/about')
-def about():
-    return 'About'
+        if moeda_origem not in moedas_validas or moeda_destino not in moedas_validas:
+            mensagem = "Moeda inválida. As moedas válidas são: " + ", ".join(moedas_validas)
+            return render_template("conversor.html", mensagem=mensagem)
+        else:
+            url = f"https://api.exchangerate-api.com/v4/latest/{moeda_origem}"
+            resposta = requests.get(url)
+            data = resposta.json()
+            taxa_cambio = data["rates"][moeda_destino]
+            valor_convertido = round(valor * taxa_cambio, 2)
+
+            return render_template("conversor.html", valor_convertido=valor_convertido, moeda_destino=moeda_destino)
+
+    return render_template("conversor.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
